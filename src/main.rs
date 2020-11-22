2fa0e269bfde7a9e13bec07663bf8e6ee3f2e70e
Arunscape <arun@woosaree.xyz>
2020-11-22 01:09:01 -0700

    no need for _ in match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(half_open_range_patterns)]
 #![feature(exclusive_range_pattern)]
+#![feature(precise_pointer_size_matching)]
 
 use std::env;
 use std::process::exit;
@@ -13,12 +14,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
+        0 => run::run_prompt(),
+        1 => run::run_file(&args[0]),
         (2..) => {
             println!("Usage: alox [script]");
             exit(64);
         }
-        1 => run::run_file(&args[0]),
-        0 => run::run_prompt(),
-        _ => unreachable!()
     }
 }
